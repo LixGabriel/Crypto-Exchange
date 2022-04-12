0146a9f25d0bbcb605fa4cfbc60986d86ad98e8e
unknown <liviu.lix20@gmail.com>
2022-04-12 21:27:43 +0300

    variabile preturi

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const totalBalance = Math.floor( (Math.random() * 1000) + 10000);
 
+const bitcoinPrice = Math.floor( (Math.random() * 100) + 100);
+const ethereumPrice = Math.floor( (Math.random() * 100) + 100);
+const elrondPrice = Math.floor( (Math.random() * 100) + 100);
+const polkadotPrice = Math.floor( (Math.random() * 100) + 100);
+const tetherPrice = 100;
